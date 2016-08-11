03ae7c2eb80c8c997fb64b773ae060ac8643c05a
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-08-11 20:28:33 +0900

    SplitView

---
+++ Source/Crux/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace Crux
+{
+    /// <summary>
+    ///     それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
