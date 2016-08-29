8ebc19061abe02d269021064ef9e76d3712d0f94
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-08-30 03:32:34 +0900

    ヘルパー追加

---
+++ Source/Crux/Models/NicoLive.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+using Microsoft.Practices.ObjectBuilder2;
+
+using Mntone.Nico2;
+using Mntone.Nico2.Live.OnAirStreams;
+
+namespace Crux.Models
+{
+    public class NicoLive : ISupportIncrementalLoading
+    {
+        private readonly NiconicoContext _context;
+        private uint _counter;
+        public ObservableCollection<OnAirStream> LiveBroadcasts { get; }
+
+        public NicoLive(NiconicoContext context)
+        {
+            _context = context;
+            LiveBroadcasts = new ObservableCollection<OnAirStream>();
+            HasMoreItems = true;
+        }
+
+        public async Task SyncLivesAsync() => await SyncLives();
+
+        private async Task SyncLives()
+        {
+            var broadcasts = await _context.Live.GetOnAirStreamsIndexAsync(0);
+            broadcasts.OnAirStreams.ForEach(w => LiveBroadcasts.Add(w));
+            _counter = (uint) broadcasts.OnAirStreams.Count;
+            if (_counter == 0)
+                HasMoreItems = false;
+        }
+
+        #region Implementation of ISupportIncrementalLoading
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            return Task.Run(async () =>
+            {
+                await SyncLives();
+                return new LoadMoreItemsResult {Count = _counter};
+            }).AsAsyncOperation();
+        }
+
+        public bool HasMoreItems { get; private set; }
+
+        #endregion
+    }
+}
--- Source/Crux/ViewModels/MainPageViewModel.cs
+++ Source/Crux/ViewModels/MainPageViewModel.cs
@@ -1,15 +1,32 @@
-﻿using Crux.Mvvm;
+﻿using System.Collections.Generic;
+
+using Crux.Models;
+using Crux.Mvvm;
 using Crux.Services.Interfaces;
+
+using Prism.Windows.Navigation;
 
 namespace Crux.ViewModels
 {
     public class MainPageViewModel : ViewModel
     {
         private readonly IAccountService _accountService;
+        private readonly NicoLive _nicoLive;
 
         public MainPageViewModel(IAccountService accountService)
         {
             _accountService = accountService;
+            _nicoLive = new NicoLive(_accountService.CurrentContext);
         }
+
+        #region Overrides of ViewModelBase
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            _nicoLive.LiveBroadcasts.Clear();
+            base.OnNavigatedTo(e, viewModelState);
+        }
+
+        #endregion
     }
 }
