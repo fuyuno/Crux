c52f9e25a75e8b4da44362356a059dba0ee7ba1a
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-08-31 14:07:52 +0900

    共通部分切り出し

---
+++ Source/Crux/Views/Controls/BroadcastList.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using Crux.Collections;
+using Crux.ViewModels.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Crux.Views.Controls
+{
+    public sealed partial class BroadcastList : UserControl
+    {
+        public BroadcastList()
+        {
+            InitializeComponent();
+        }
+
+        #region Header
+
+        public static readonly DependencyProperty HeaderProperty
+            = DependencyProperty.Register(nameof(Header), typeof(FrameworkElement), typeof(BroadcastList),
+                                          new PropertyMetadata(null, HeaderPropertyChangedCallback));
+
+        public FrameworkElement Header
+        {
+            get { return (FrameworkElement) GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        private static void HeaderPropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = sender as BroadcastList;
+            if (obj != null)
+                obj.Header = (FrameworkElement) e.NewValue;
+        }
+
+        #endregion
+
+        #region ItemsSource
+
+        public static readonly DependencyProperty ItemsSourceProperty
+            = DependencyProperty.Register(nameof(ItemsSource), typeof(IncrementalObservableCollection<BroadcastProgramViewModel>),
+                                          typeof(BroadcastList), new PropertyMetadata(null, ItemsSourcePropertyChangedCallback));
+
+        public IncrementalObservableCollection<BroadcastProgramViewModel> ItemsSource
+        {
+            get { return (IncrementalObservableCollection<BroadcastProgramViewModel>) GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        private static void ItemsSourcePropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = sender as BroadcastList;
+            if (obj != null)
+                obj.ItemsSource = (IncrementalObservableCollection<BroadcastProgramViewModel>) e.NewValue;
+        }
+
+        #endregion
+    }
+}
