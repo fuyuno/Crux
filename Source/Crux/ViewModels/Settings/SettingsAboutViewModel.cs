3ee40818f7d1001bcd21454964390a4ad7d49ba4
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-09-04 04:22:09 +0900

    フィードバック Hub へ

---
+++ Source/Crux/ViewModels/Settings/SettingsAboutViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Crux.Mvvm;
+
+using Microsoft.Services.Store.Engagement;
+
+namespace Crux.ViewModels.Settings
+{
+    public class SettingsAboutViewModel : ViewModel
+    {
+        public async void OnClickFeedback()
+        {
+            var launcher = StoreServicesFeedbackLauncher.GetDefault();
+            await launcher.LaunchAsync();
+        }
+    }
+}
--- Source/Crux/Views/Settings/SettingsAboutView.xaml.cs
+++ Source/Crux/Views/Settings/SettingsAboutView.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using Windows.UI.Xaml.Controls;
 
+using Crux.ViewModels.Settings;
+
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Crux.Views.Settings
 {
     public sealed partial class SettingsAboutView : UserControl
     {
+        public SettingsAboutViewModel ViewModel => DataContext as SettingsAboutViewModel;
+
         public SettingsAboutView()
         {
             InitializeComponent();
